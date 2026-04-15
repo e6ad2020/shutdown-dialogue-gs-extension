8dfe50ce9acbcc54520e71463fb01bc9162b2a9e
Eyad <e6ad2020@users.noreply.github.com>
2026-04-15 16:43:47 +0200

    fix: exclude po files from zip and compile them in build script to satisfy Shexli

--- build.sh
+++ build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-cd power-menu@e6ad2020
+# Define extension name and folder
+EXT_ID="power-menu@e6ad2020"
 
-zip -r ../power-menu@e6ad2020.zip . -x "schemas/gschemas.compiled"
+cd "$EXT_ID"
+
+# 1. Compile translations into the local locale folder
+# This ensures translations work without shipping .po source files
+mkdir -p locale/ar/LC_MESSAGES
+msgfmt po/ar.po -o "locale/ar/LC_MESSAGES/$EXT_ID.mo"
+
+# 2. Build the ZIP file
+# -x excludes files that are unnecessary or should not be shipped for review
+zip -r "../$EXT_ID.zip" . \
+    -x "schemas/gschemas.compiled" \
+    -x "po/*" \
+    -x "*.sh" \
+    -x ".gitignore" \
+    -x ".git/*"
+
+echo "Build complete: $EXT_ID.zip"
